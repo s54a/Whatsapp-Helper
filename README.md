292e26a28d700b01d56ac5cfc9f94443beffb82f
Sooraj Gupta <soorajgupta00@gmail.com>
2024-05-25 20:50:19 +0530

    v3

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Whatsapp-Helper
-It allows you to message anyone on Whatsapp without saving their phone number 
+
+It allows you to message anyone on Whatsapp without saving their Phone Number.
+
+You can also add a Message which is optional and use it to create HyperLinks for your Business or Etc
+
+I have used Particles by [Vincent Garreau](https://github.com/VincentGarreau/particles.js)
+
+## Screen Shot
+
+![Whatsapp Helper](image.png)
+
+## Deployed Link
+
+[https://whatsapphelper.netlify.app/](https://whatsapphelper.netlify.app/)
---
+++ main.js
@@ -0,0 +1,84 @@
+import "./style.css";
+import "./particle";
+
+const phoneNumberInput = document.getElementById("phoneNumber");
+const messageInput = document.getElementById("message");
+const linkContainer = document.getElementById("linkContainer");
+const copyButton = document.getElementById("copyButton");
+const openChatButton = document.getElementById("openChatButton");
+
+phoneNumberInput.addEventListener("input", updateLink);
+messageInput.addEventListener("input", updateLink);
+copyButton.addEventListener("click", copyLink);
+openChatButton.addEventListener("click", openChat);
+
+function updateLink() {
+  let phoneNumber = phoneNumberInput.value.trim();
+  const message = encodeURIComponent(messageInput.value.trim());
+  let countryCode = "91";
+
+  // Clear link if phone number input is empty
+  if (phoneNumber === "") {
+    linkContainer.innerHTML = "";
+    return;
+  }
+
+  // Validate phone number length
+  if (phoneNumber.length < 4 || phoneNumber.length > 15) {
+    linkContainer.innerHTML = "Phone number must be between 4 and 15 digits.";
+    return false;
+  }
+
+  // Check if country code is provided or not
+  if (phoneNumber.length > 10) {
+    countryCode = phoneNumber.substring(0, phoneNumber.length - 10);
+    phoneNumber = phoneNumber.substring(phoneNumber.length - 10);
+  }
+
+  const whatsappLink = `https://wa.me/${countryCode}${phoneNumber}${
+    message ? `?text=${message}` : ""
+  }`;
+  linkContainer.innerHTML = `<a href="${whatsappLink}" target="_blank">${whatsappLink}</a>`;
+  linkContainer.style.margin = "1rem";
+}
+
+function copyLink() {
+  const link = linkContainer.querySelector("a");
+
+  if (link) {
+    const tempInput = document.createElement("input");
+
+    tempInput.value = link.href;
+
+    document.body.appendChild(tempInput);
+
+    tempInput.select();
+
+    document.execCommand("copy");
+
+    document.body.removeChild(tempInput);
+
+    alert("Link copied to clipboard!");
+  } else {
+    alert("Please enter a valid phone number.");
+  }
+}
+
+function openChat() {
+  const link = linkContainer.querySelector("a");
+  if (link) {
+    window.open(link.href, "_blank");
+  } else {
+    alert("Please enter a valid phone number.");
+  }
+}
+
+function toggleHidden() {
+  const hiddenContent = document.querySelector(".hidden");
+  hiddenContent.classList.toggle("visible");
+
+  // Scroll to the hidden content
+  if (hiddenContent.classList.contains("visible")) {
+    hiddenContent.scrollIntoView({ behavior: "smooth" });
+  }
+}
